90f20b652f3df06454702d6be1769bc2b54fa2a3
yaofei <yaofei@wzdai.com>
2018-09-12 16:23:30 +0800

    添加安装中文字符，中国时区，java环境，docker环境，swoole扩展，开启启动服务

--- lnmp-single/setup.sh
+++ lnmp-single/setup.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 
 # install some tools
-sudo yum install -y git wget curl vim gcc glibc-static telnet bridge-utils
+sudo yum install -y git wget curl vim gcc glibc-static telnet bridge-utils net-tools lrzsz zip unzip kde-l10n-Chinese
 
 # set aliyun yum source
 sudo mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.bak
@@ -11,7 +11,7 @@ sudo yum clean all
 sudo yum makecache
 sudo yum -y update
 
-sudo yum install -y tcl tcl-devel expect
+# set github hosts map 
 
 sudo sh -c 'echo "192.30.252.123 www.github.com" >>  /etc/hosts'
 sudo sh -c 'echo "192.30.253.113 github.com" >>  /etc/hosts'
@@ -21,18 +21,9 @@ sudo sh -c 'echo "185.31.18.133 avatars0.githubusercontent.com" >>  /etc/hosts'
 sudo sh -c 'echo "185.31.19.133 avatars1.githubusercontent.com" >>  /etc/hosts'
 sudo sh -c 'echo "52.206.76.142 collector.githubapp.com" >>  /etc/hosts'
 
-# install php 7.x
-sudo yum install -y epel-release
-sudo rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
-sudo yum install -y --skip-broken mod_php72w php72w-*
-
-# install nginx
-sudo yum install -y nginx
-
-# install mysql images
-
-
 # set develop user
+sudo yum install -y tcl tcl-devel expect
+
 user="develop"
 password="123456"
 sudo groupadd $user
@@ -48,6 +39,81 @@ send "${password}\r"
 expect eof;
 EOF
 
-sudo mv /etc/ssh/sshd_config /etc/ssh/sshd_config.bk
-sudo cp /vagrant/sshd_config /etc/ssh/sshd_config
+#update ssh login config
+sudo sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
 sudo systemctl restart sshd
+
+#install chinese charset
+sudo yum reinstall -y glibc-common
+sudo \cp  /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+sudo sh -c "echo 'LANG=\"zh_CN.UTF-8\"' > /etc/locale.conf"
+
+#install java jdk development
+sudo yum install -y java-1.8.0-openjdk java-1.8.0-openjdk-devel
+sudo sh -c 'echo "export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk" >> /etc/profile'
+sudo sh -c 'echo "export JRE_HOME=${JAVA_HOME}/jre" >> /etc/profile'
+sudo sh -c 'echo "export CLASSPATH=.:${JAVA_HOME}/lib:${JRE_HOME}/lib:$JRE_HOME/lib/rt.jar:$JAVA_HOME/lib/tools.jar" >> /etc/profile'
+sudo sh -c 'echo "export PATH=$PATH:$JAVA_HOME/bin" >> /etc/profile'
+sudo sh -c 'echo "export TIME_STYLE=\"+%Y/%m/%d %H:%M:%S\"" >> /etc/profile'
+sudo source /etc/profile
+
+# install docker
+sudo yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-selinux docker-engine-selinux docker-engine
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+sudo yum-config-manager -y --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install -y docker-ce
+
+# sudo sh -c 'echo "{\"registry-mirrors\": [\"https://registry.docker-cn.com\"]}" > /etc/docker/daemon.json'
+
+# start docker service
+sudo groupadd docker
+sudo usermod -aG docker vagrant
+
+# install php 7.x
+sudo yum install -y epel-release
+sudo rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
+sudo yum install -y --skip-broken mod_php71w php71w-*
+
+# install hiredis
+sudo wget https://github.com/redis/hiredis/archive/v0.13.3.tar.gz
+sudo tar xzvf v0.13.3.tar.gz && cd hiredis-0.13.3/ && sudo make -j && sudo make install && sudo ldconfig && cd ..
+
+# install nghttp2
+sudo wget https://github.com/nghttp2/nghttp2/releases/download/v1.33.0/nghttp2-1.33.0.tar.gz
+sudo tar zxvf nghttp2-1.33.0.tar.gz && cd nghttp2-1.33.0/ && sudo ./configure && sudo make && sudo make install && cd ..
+
+# install swoole devel
+sudo yum install -y pcre-devel glibc-headers gcc-c++ openssl-devel
+sudo wget https://github.com/swoole/swoole-src/archive/v4.1.2.tar.gz
+sudo tar zxvf v4.1.2.tar.gz && cd swoole-src-4.1.2 
+sudo ./configure  --enable-openssl --enable-http2 --enable-async-redis --enable-sockets --enable-mysqlnd && sudo make && sudo make install && cd ..
+sudo sh -c 'echo "/usr/local/lib" >> /etc/ld.so.conf' && sudo ldconfig
+sudo sh -c 'echo "extension=swoole.so" > /etc/php.d/swoole.ini'
+
+# install nginx
+sudo yum install -y nginx
+sudo sh -c 'echo "docker-node2" >  /usr/share/nginx/html/index.html'
+
+#create web user www
+sudo groupadd www
+sudo useradd www -g www
+
+sudo sed -i "s/user nginx/user www/g"  /etc/nginx/nginx.conf
+sudo \cp /vagrant/default.conf /etc/nginx/conf.d/default.conf
+
+sudo sed -i "s/user = apache/user = www/g"  /etc/php-fpm.d/www.conf
+sudo sed -i "s/group = apache/group = www/g"  /etc/php-fpm.d/www.conf
+sudo sed -i "s/;listen.owner = nobody/listen.owner = www/g"  /etc/php-fpm.d/www.conf
+
+#set start service
+#先关闭selinux，否者无法通过systemctl启动nginx
+sudo setenforce 0
+sudo sed -i "s/SELINUX=enforcing/SELINUX=disabled/g"  /etc/selinux/config
+
+sudo systemctl enable php-fpm
+sudo systemctl enable nginx
+sudo systemctl enable docker
+
+sudo halt
+
+
